c9e59e555887dbc35218a649ebcaabb517d9d7a1
Nevio <0x19@users.noreply.github.com>
2016-05-16 20:36:24 +0200

    Removing return_score in favor of return_action.

--- sift.go
+++ sift.go
@@ -20,14 +20,6 @@ func (s *Sift) Track(event string, params map[string]interface{}, args map[strin
 	// name (that does not start with a $).
 	params["$type"] = event
 
-	// Whether the API response should include a score for this
-	// user (the score will be calculated using this event).  This feature must be
-	// enabled for your account in order to use it.  Please contact
-	// support@siftscience.com if you are interested in using this feature.
-	if score, ok := args["return_score"]; ok {
-		params["return_score"] = score.(bool)
-	}
-
 	// Whether the API response should include actions in the response. For
 	// more information on how this works, please visit the tutorial at:
 	// https://siftscience.com/resources/tutorials/formulas
